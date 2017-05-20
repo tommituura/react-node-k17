caa0b901e3400266fe5e1960981c288b78f6ac0d
Tommi Tuura <tommi.tuura@gmail.com>
2017-05-20 12:26:43 +0300

    stuff

--- app/server.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-var port = 80;
-
-http.createServer(function(req, res) {
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.write("Hello world from Node.js");
-    res.end();
-}).listen(port);
